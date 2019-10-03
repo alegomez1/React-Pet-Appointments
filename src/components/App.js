e51a4ad613ab9ab07c7bf31a993c063033d6a710
alejandrogomez <alegomez1@me.com>
2019-10-02 21:23:25 -0400

    Changed App,js to class, added constructor

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,27 @@
-import React from "react";
+import React, {Component} from "react";
 import "../css/App.css";
 import AddAppointments from "../components/AddAppointments";
 import ListAppointments from "../components/ListAppointments";
 import SearchAppointments from "../components/SearchAppointments";
+import { tsConstructorType } from "@babel/types";
 
 
-function App() {
+class App extends Component{
+
+  constructor(){
+    super();
+    this.state={
+      myName: 'Alex'
+    }
+  }
+  render(){
   return (
     <main className="page bg-white" id="petratings">
       <div className="container">
         <div className="row">
           <div className="col-md-12 bg-white">
             <div className="container">
+              {this.state.myName}
               <AddAppointments />
               <ListAppointments />
               <SearchAppointments />
@@ -21,6 +31,7 @@ function App() {
       </div>
     </main>
   );
+}
 }
 
 export default App;
